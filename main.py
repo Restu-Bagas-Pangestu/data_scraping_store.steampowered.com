4524397f734285fff3aaaabe8064a9df826b13fc
RBAGASPANGES <restu.bagas.pangestu@gmail.com>
2023-10-01 22:34:37 +0700

    scraping code

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 """
 Data scraping store.steampowered.com aplication
 """
-import requests
-from bs4 import BeautifulSoup
-import json
-import pandas as pd
+import requests #ekstraksi data
+from bs4 import BeautifulSoup #processing data
+import json #file to excel
+import pandas as pd #dataframe
 
 #extraction data:
 url = 'https://store.steampowered.com/search'
@@ -21,13 +21,13 @@ def parse(data):
 
     #print(result)
 
-    for game in games:
-        link = game['href'] #untuk redirect ke link tersebut
+    for list_games in games:
+        link = list_games['href'] #untuk redirect ke link tersebut
 
     #parsing data
-        title = game.find('span',{'class':'title'}).text.strip().split('£')[0]
-        price = game.find('div',{'class':'search_discount_and_price'}).text.strip().split('£')[0]
-        released = game.find('div',{'class':'search_released'}).text.strip().split('£')[0]
+        title = list_games.find('span',{'class':'title'}).text.strip().split('£')[0]
+        price = list_games.find('div',{'class':'search_discount_and_price'}).text.strip().split('£')[0]
+        released = list_games.find('div',{'class':'search_released'}).text.strip().split('£')[0]
 
         if released == '':
             released='none'
@@ -43,17 +43,32 @@ def parse(data):
         #append
         result.append(data_dict)
     return result
+    #writing json
+    with open ('json_result.json', 'w') as outfile:
+        json.dump(result,outfile)
+    return result
+
+#read json
+def load_data():
+    with open('json_result.json') as json_file:
+        data = json.load(json_file)
+
 
 #proscess cleaned data from parser
 def output (diatas: list):
     for i in diatas:
         print(i)
 
+def generate_data(result, file_name):
+    df=pd.DataFrame(result)
+    df.to_excel(f'{file_name}.xlsx', index=False)
+
 if __name__ == '__main__':
     data = get_data(url)
 
     final_data = parse(data)
-
+    file_name= input('masukan nama file:')
+    generate_data(final_data,file_name)
     output(final_data)
 
 
